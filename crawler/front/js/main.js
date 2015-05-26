8d9e8bf26a6206cc4b7a8430d155b9852c6b5dbe
IlanMa <Ilan.mamontov@gmail.com>
2015-05-26 18:03:31 -0400

    update code

--- crawler/front/js/main.js
+++ crawler/front/js/main.js
@@ -1,37 +1,48 @@
 var app = angular.module('SpotOnIt', []);
 
-app.controller('LayoutCtrl', function($scope, $http){
+app.controller('LayoutCtrl', ['$scope', '$http', function($scope, $http) {
     
-    $http.get('https://demo-project-edibly.c9.io/crawl').
-        success(function(data, status, headers, config) {
-            cont = data;
-        }).
-        error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        });    
-	
-	$scope.events = [
-	{title: "This is a random event", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "This could also possibly be an event", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Montreal Canadiens stanley cup parade", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "San Jose Sharks relocating to New Mexico", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Joe Thornton's first game in a habs Jersey", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "This is a random event", start: "4:20" , date: "Apr 21", end: "5:00", location: "Location"},
-	{title: "This could also possibly be an event", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Montreal Canadiens stanley cup parade", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "San Jose Sharks relocating to New Mexico", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Joe Thornton's first game in a habs Jersey", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "This is a random event", start: "4:20" , date: "Apr 21", end: "5:00", location: "Location"},
-	{title: "This could also possibly be an event", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Montreal Canadiens stanley cup parade", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "San Jose Sharks relocating to New Mexico", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Joe Thornton's first game in a habs Jersey", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "This is a random event", start: "4:20" , date: "Apr 21", end: "5:00", location: "Location"},
-	{title: "This could also possibly be an event", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Montreal Canadiens stanley cup parade", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "San Jose Sharks relocating to New Mexico", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	{title: "Joe Thornton's first game in a habs Jersey", date: "Apr 21", start: "4:20" , end: "5:00", location: "Location"},
-	];
+    $scope.crawl = function(url) {
 
-})
+        $http.get('https://salty-peak-4499.herokuapp.com/crawl?url=' + url).   // get HTML of URL to crawl
+            success(function(data, status, headers, config) {
+                
+                var parsedData = $.parseHTML(data);  // create jQuery object from HTML          
+                
+                $scope.events = {};
+                var monthRegex = "*:contains('January'):not(:has(*)), *:contains('Jan'):not(:has(*)), " +
+                                 "*:contains('February'):not(:has(*)), *:contains('Feb'):not(:has(*)), " +
+                                 "*:contains('March'):not(:has(*)), *:contains('Mar'):not(:has(*)), " +
+                                 "*:contains('April'):not(:has(*)), *:contains('Apr'):not(:has(*)), " +
+                                 "*:contains('May'):not(:has(*)), " +
+                                 "*:contains('June'):not(:has(*)), *:contains('Jun'):not(:has(*)), " +
+                                 "*:contains('July'):not(:has(*)), *:contains('Jul'):not(:has(*)), " +
+                                 "*:contains('August'):not(:has(*)), *:contains('Aug'):not(:has(*)), " +
+                                 "*:contains('September'):not(:has(*)), *:contains('Sep'):not(:has(*)), " +
+                                 "*:contains('October'):not(:has(*)), *:contains('Oct'):not(:has(*)), " +
+                                 "*:contains('November'):not(:has(*)), *:contains('Nov'):not(:has(*)), " +
+                                 "*:contains('December'):not(:has(*)), *:contains('Dec'):not(:has(*))"; // find element with month in it
+                
+                $(parsedData).find(monthRegex).each(function(index, el) {
+                
+                    var event = {};
+                    event.date = $(el).text();
+                    event.location = "Somewhere";
+                    
+                    var parent = $(el).parent();
+                    
+                    while($(parent).find("h3").length == 0)
+                        parent = $(parent).parent();
+                        
+                    event.title = $(parent).find("h3").text(); // go up     
+                                                  
+                    $scope.events[index] = event;                
+                });     
+                
+                
+            }).
+            error(function(data, status, headers, config) {
+                console.log("HTML fetch failed!");
+            });
+    }    
+}]);
